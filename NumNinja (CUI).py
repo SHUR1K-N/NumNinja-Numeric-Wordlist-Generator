df6d8db4c3588568cf7081ad33dae988f60d7560
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-07-24 16:56:13 +0530

    Fixed text guide

--- NumNinja (CUI).py
+++ NumNinja (CUI).py
@@ -120,15 +120,15 @@ while (correctInput is False):
                     break
 
         elif (minunit == maxunit):
-            print("\nThe minimum value cannot be equal to the maximum value.")
+            print("\nThe minimum value cannot be equal to the maximum value. Please try again.\n")
 
         elif (minunit > maxunit):
-            print("\nThe minimum value cannot be greater than the maximum value.")
+            print("\nThe minimum value cannot be greater than the maximum value. Please try again.\n")
     except ZeroDivisionError:
         print("\n\nThe task completed successfully in zero seconds.")
         print("Press any key to exit.")
         input()
         break
     except:
-        print("\nOne of more of the inputs are invalid. This can happen when any spaces or other characters have been entered instead of numbers. Please try again.")
+        print("\nOne of more of the inputs are invalid. This can happen when any spaces or other characters have been entered instead of numbers. Please try again.\n")
         continue
